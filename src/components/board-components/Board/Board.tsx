c8089b428c906cced8e62cda34416a1eeb8dcf2c
shakthi.m <shakthi.m@go-mmt.com>
2024-07-01 00:05:48 +0530

    rolled back to div input

--- src/components/board-components/Board/Board.tsx
+++ src/components/board-components/Board/Board.tsx
@@ -1,49 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import styles from './Board.module.scss';
+import styles from "./Board.module.scss";
 
-import Row from '@/components/board-components/Row/Row';
-import { useGame } from '@/components/Game/GameProvider';
+import Row from "@/components/board-components/Row/Row";
+import {useGame} from "@/components/Game/GameProvider";
 
 interface BoardProps {}
 
-const Board: React.FC<BoardProps> = (props) => {
-  const { board, handleBoardKeyPress } = useGame();
+const Board : React.FC<BoardProps> = (props) => {
+    const {board, handleBoardKeyPress } = useGame();
 
-  const inputRef = React.useRef<HTMLInputElement | null>(null);
+    const divRef = React.useRef<HTMLDivElement | null>(null);
+    React.useEffect(() => {
+        if (divRef.current) {
+            if ("focus" in divRef.current) {
+                divRef.current.focus();
+            }
+        }
+    }, [board]);
 
-  const handleBoardTouch = () => {
-    if (inputRef.current) {
-      if ('focus' in inputRef.current) {
-        inputRef.current.focus();
-      }
-    }
-  };
-
-  React.useEffect(() => {
-    if (inputRef.current) {
-      if ('focus' in inputRef.current) {
-        inputRef.current.focus();
-      }
-    }
-  }, []);
-
-  return (
-    <div className={styles['board']} onClick={handleBoardTouch}>
-      <input
-        ref={inputRef}
-        type="text"
-        autoComplete="off"
-        tabIndex={0}
-        className={styles['hidden-input']}
-        inputMode={'text'}
-        onKeyPress={handleBoardKeyPress}
-      />
-      {board.map((row, rowIndex) => (
-        <Row key={rowIndex} rowIndex={rowIndex} rowData={row} />
-      ))}
-    </div>
-  );
-};
+    return(
+        <div className={styles['board']} tabIndex={0}  ref={divRef} onKeyDown={handleBoardKeyPress}>
+            {board.map((row, rowIndex) => (
+                <Row key={rowIndex} rowIndex={rowIndex} rowData={row}/>
+            ))}
+        </div>
+    );
+}
 
 export default Board;

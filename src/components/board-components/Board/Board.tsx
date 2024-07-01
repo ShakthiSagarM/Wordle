bfe732cc2f8c7042bc6e071848302273d4ad0fa0
shakthi.m <shakthi.m@go-mmt.com>
2024-07-02 01:13:38 +0530

    pulsing effect scaling added

--- src/components/board-components/Board/Board.tsx
+++ src/components/board-components/Board/Board.tsx
@@ -1,31 +1,31 @@
-import React from "react";
+import React from 'react';
 
-import styles from "./Board.module.scss";
+import styles from './Board.module.scss';
 
-import Row from "@/components/board-components/Row/Row";
-import {useGame} from "@/components/Game/GameProvider";
+import Row from '@/components/board-components/Row/Row';
+import { useGame } from '@/components/Game/GameProvider';
 
 interface BoardProps {}
 
-const Board : React.FC<BoardProps> = (props) => {
-    const {board, handleBoardKeyPress } = useGame();
+const Board: React.FC<BoardProps> = () => {
+  const { board, handleBoardKeyPress } = useGame();
 
-    const divRef = React.useRef<HTMLDivElement | null>(null);
-    React.useEffect(() => {
-        if (divRef.current) {
-            if ("focus" in divRef.current) {
-                divRef.current.focus();
-            }
-        }
-    }, [board]);
+  const divRef = React.useRef<HTMLDivElement | null>(null);
+  React.useEffect(() => {
+    if (divRef.current) {
+      if ('focus' in divRef.current) {
+        divRef.current.focus();
+      }
+    }
+  }, [board]);
 
-    return(
-        <div className={styles['board']} tabIndex={0}  ref={divRef} onKeyDown={handleBoardKeyPress}>
-            {board.map((row, rowIndex) => (
-                <Row key={rowIndex} rowIndex={rowIndex} rowData={row}/>
-            ))}
-        </div>
-    );
-}
+  return (
+    <div className={styles['board']} tabIndex={0} ref={divRef} onKeyDown={handleBoardKeyPress}>
+      {board.map((row, rowIndex) => (
+        <Row key={rowIndex} rowIndex={rowIndex} rowData={row} />
+      ))}
+    </div>
+  );
+};
 
 export default Board;
--- src/components/ui-components/PulsingEffect/PulsingEffect.tsx
+++ src/components/ui-components/PulsingEffect/PulsingEffect.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import styles from './PulsingEffect.module.scss';
 import { useGame } from '@/components/Game/GameProvider';
 
 const PulsingEffect: React.FC = () => {
   const { config, featureState, timeLeft, isGameOver } = useGame();
 
+  const pulsingSize = useMemo(() => {
+    const maxSize = 20;
+    const minSize = 5;
+    const sizeRange = maxSize - minSize;
+    const adjustedSize = minSize + sizeRange * (1 - timeLeft / config.pulsingEffectStartFrom);
+    return `${adjustedSize}rem`;
+  }, [timeLeft, config.pulsingEffectStartFrom]);
+
   if (!featureState.showPulsingEffect || timeLeft > config.pulsingEffectStartFrom || timeLeft <= 0 || isGameOver) {
     return null;
   }
 
-  return <div className={styles['pulsing-effect']}></div>;
+  return (
+    <div className={styles['pulsing-effect']} style={{ '--pulsing-size': pulsingSize } as React.CSSProperties}></div>
+  );
 };
 
 export default PulsingEffect;
